033f0b8eddeba4873bd1b9b3bd6badb41c807471
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-03-01 17:15:38 +0530

    Adding script for navigating between sections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-$(document).ready(function(){
-    $("#men-outer").click(function(){
+$(document).ready(function () {
+    //navigating to mens outerwear category
+    $("#men-outer, #men-outer-img, #men-outer-category .button").click(function () {
         $("#ladies-outer-category").hide();
+        $(".tshirts").hide();
+        $(".men-outerwear").show();
+        $('#men-outer-category .button, #men-outer-category #men-outer-title').hide();
+    });
+    //home button functionality
+    $('.home-button').click(function(){
+        $("#men-outer-category").show();
+        $('.men-outerwear-product').hide();
+        $(".men-outerwear").hide();
+        $("#ladies-outer-category").show();
+        $(".tshirts").show();
+        $('#men-outer-category .button, #men-outer-category #men-outer-title').show();
+
+    });
+    //navigating to product details page
+    $('.product-item').click(function(){
+        $('.men-outerwear-product').css("display", "flex");
+        $(".men-outerwear").hide();
+        $("#men-outer-category").hide();
     });
 });
