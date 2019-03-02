ad518f6f6506fb02da639c463adeebcb0da617cb
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-03-02 12:22:50 +0530

    changed class names for product list section and product details section

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ $(document).ready(function () {
         $("#ladies-outer-category").hide();
         $('#men-outer-category').show();
         $(".tshirts").hide();
-        $(".men-outerwear").show();
+        $(".product-list-section").show();
         $('#men-outer-category .button, #men-outer-category #men-outer-title').hide();
-        $('.men-outerwear-product').hide();
+        $('.product-details.section').hide();
     });
     //navigating to ladies outerwear category
     $("#ladies-outer, #ladies-outer-img, #ladies-outer-category .button").click(function () {
         $("#men-outer-category").hide();
         $('#ladies-outer-category').show();
         $(".tshirts").hide();
-        $(".men-outerwear").show();
+        $(".product-list-section").show();
         $('#ladies-outer-category .button, #ladies-outer-category #ladies-outer-title').hide();
-        $('.men-outerwear-product').hide();
+        $('.product-details-section').hide();
     });
     //navigating to men tshirts category
     $("#men-tshirt, #men-tshirt-img, #men-tshirt-category .button").click(function () {
@@ -24,9 +24,9 @@ $(document).ready(function () {
         $("#ladies-outer-category").hide();
         $(".tshirts #ladies-tshirt-category").hide();
         $(".tshirts #men-tshirt-category").show();
-        $(".men-outerwear").show();
+        $(".product-list-section").show();
         $('#men-tshirt-category .button, #men-tshirt-category #men-tshirt-title').hide();
-        $('.men-outerwear-product').hide();
+        $('.product-details-section').hide();
     });
     //navigating to ladies tshirts category
     $("#ladies-tshirt, #ladies-tshirt-img, #ladies-tshirt-category .button").click(function () {
@@ -35,15 +35,15 @@ $(document).ready(function () {
         $("#ladies-outer-category").hide();
         $(".tshirts #men-tshirt-category").hide();
         $(".tshirts #ladies-tshirt-category").show();
-        $(".men-outerwear").show();
+        $(".product-list-section").show();
         $('#ladies-tshirt-category .button, #ladies-tshirt-category #ladies-tshirt-title').hide();
-        $('.men-outerwear-product').hide();
+        $('.product-details-section').hide();
     });
     //home button functionality
     $('.home-button').click(function () {
         $("#men-outer-category").show();
-        $('.men-outerwear-product').hide();
-        $(".men-outerwear").hide();
+        $('.product-details-section').hide();
+        $(".product-list-section").hide();
         $("#ladies-outer-category").show();
         $(".tshirts, .tshirts .tshirt-item, .tshirt-item .tshirt-title, .tshirt-item .button").show();
         $('#men-outer-category .button, #men-outer-category #men-outer-title').show();
@@ -52,8 +52,8 @@ $(document).ready(function () {
     });
     //navigating to product details page
     $('.product-item').click(function () {
-        $('.men-outerwear-product').css("display", "flex");
-        $(".men-outerwear").hide();
+        $('.product-details-section').css("display", "flex");
+        $(".product-list-section").hide();
         $("#men-outer-category").hide();
         $("#ladies-outer-category").hide();
         $("#men-tshirt-category").hide();
