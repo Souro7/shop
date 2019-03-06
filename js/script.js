bfefcf80a6055d55d263eb3bf6c1aec91c21eb47
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-03-06 17:45:01 +0530

    Adding checkout page

--- js/script.js
+++ js/script.js
@@ -12,9 +12,11 @@ $(document).ready(function () {
         $('.product-item-name').text("Generic Men's Outerwear");
         $('.product-item-img img').attr('src', 'images/product_images/men_outerwear_.jpg');
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         categoryFlag = 1;
     });
+
     //navigating to ladies outerwear category
     $("#ladies-outer, #ladies-outer-img, #ladies-outer-category .button").click(function () {
         $("#men-outer-category").hide();
@@ -27,10 +29,12 @@ $(document).ready(function () {
         $('.product-item-name').text("Generic Ladies Outerwear");
         $('.product-item-img img').attr('src', 'images/product_images/ladies_outerwear.jpg');
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         categoryFlag = 2;
 
     });
+
     //navigating to men tshirts category
     $("#men-tshirt, #men-tshirt-img, #men-tshirt-category .button").click(function () {
         $("#men-outer-category").hide();
@@ -45,9 +49,11 @@ $(document).ready(function () {
         $('.product-item-name').text("Generic Men's T-shirt");
         $('.product-item-img img').attr('src', 'images/product_images/men_tshirt.jpg');
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         categoryFlag = 3;
     });
+
     //navigating to ladies tshirts category
     $("#ladies-tshirt, #ladies-tshirt-img, #ladies-tshirt-category .button").click(function () {
         $("#men-outer-category").hide();
@@ -62,9 +68,11 @@ $(document).ready(function () {
         $('.product-item-name').text("Generic Ladies T-shirt");
         $('.product-item-img img').attr('src', 'images/product_images/ladies_tshirt.jpg');
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         categoryFlag = 4;
     });
+
     //home button functionality
     $('.home-button').click(function () {
         $("#men-outer-category").show();
@@ -75,10 +83,12 @@ $(document).ready(function () {
         $('#men-outer-category .button, #men-outer-category #men-outer-title').show();
         $('#ladies-outer-category .button, #ladies-outer-category #ladies-outer-title').show();
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         categoryFlag = 0;
 
     });
+
     //navigating to product details page
     $('.product-item').click(function () {
         $('.product-details-section').css("display", "flex");
@@ -88,7 +98,8 @@ $(document).ready(function () {
         $("#men-tshirt-category").hide();
         $("#ladies-tshirt-category").hide();
         $('.cart').hide();
-        $(".menu").show();
+        $(".menu").css("display", "flex");
+        $(".checkout").hide();
         switch (categoryFlag) {
             case 1: {
                 $('.product-name').text("Generic Men's Outerwear");
@@ -112,9 +123,23 @@ $(document).ready(function () {
             }
         }
     });
+
     //navigating to cart page
     $('.cart-button').click(function () {
         $('.cart').show();
+        $('.product-details-section').hide();
+        $('.product-list-section').hide();
+        $("#men-outer-category").hide();
+        $("#ladies-outer-category").hide();
+        $(".tshirts").hide();
+        $(".menu").hide();
+        $(".checkout").hide();
+    });
+
+    //navigating to checkout page
+    $(".cart-checkout-box .button").click(function () {
+        $(".checkout").show();
+        $('.cart').hide();
         $('.product-details-section').hide();
         $('.product-list-section').hide();
         $("#men-outer-category").hide();
